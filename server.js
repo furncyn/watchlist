c70c7a0edc47180a9d6c1ac39ecd98edf84deedb
Furn Techalertumpai <furncyn@ucla.edu>
2019-09-02 18:38:22 -0700

    server.js now calls a Python scraper's script when a URL is inputted. Next step is to figure out how to use a return value from the Python script instead of pipelining the output from stdout.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+let {PythonShell} = require('python-shell')
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ const url = "mongodb+srv://furncyn:courtside5@watchlist-4bthd.gcp.mongodb.net/te
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+const scraperPath = 'scraper.py';
 
 // Connect to database and create collection
 MongoClient.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }, (err, client) => {
@@ -22,21 +25,34 @@ MongoClient.connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true }
     app.get('/', (req, res) => {
         db.collection('watchlist').find().toArray((err, result) => {
             if (err) return console.log(err)
-            console.log(result)
             res.render('index.ejs', {watchlist: result})
           })
     })
 
-    // Add input url into watchlist database
+    // Add input url into watchlist database, duplicates are ignored
     app.post('/add-item', (req, res) => {
-        db.collection('watchlist').insertOne(req.body, (err, result) => {
+        console.log('url:', req.body.url)
+        // PythonShell.run(scraperPath, {mode: 'text', args: [req.body.url]}, (err, data) => {
+        //     if (err) res.send(err)
+        //     console.log(data)
+        // })
+        let pyshell = new PythonShell(scraperPath, {mode: 'text', args: [req.body.url]})
+        pyshell.on('message', function(message) {
+            console.log(message)
+        }) 
+        pyshell.end(function(err) {
+            if (err) return console.log(err);
+            console.log('success')
+        })
+
+        db.collection('watchlist').update(req.body, req.body, {upsert: true}, (err, result) => {
             if (err) return console.log(err)
             console.log('saved to database')
             res.redirect('/')
         })
     })
 
-    // Retrieve all items in the watchlist database
+    // Retrieve all items in the watchlist database and disolay in a list
     app.get('/mywatchlist', (req, res) => {
         db.collection('watchlist').find().toArray((err, result) => {
             if (err) return console.log(err)
